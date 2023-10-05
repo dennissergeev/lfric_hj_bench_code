cd13f37d8712d71166ee971f1b14bc79a938798a
Denis Sergeev <dennis.sergeev@gmail.com>
2023-10-05 15:23:15 +0100

    add YAML with SOCRATES gases, add CLI to prep_camembert_ancil; update pyproject

--- src/scripts/prep_camembert_ancil.py
+++ src/scripts/prep_camembert_ancil.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""Prepare CAMEMBERT input data for the UM and LFRic."""
 # Standard library
 from pathlib import Path
 import sys
-from typing import Tuple
+from warnings import filterwarnings
 
 # Third party
-import ants
-import numpy as np
+import aeolus
+from aeolus.const import init_const
+from aeolus.io import create_dummy_cube, load_vert_lev
+import click
 import iris
 from iris.coords import AuxCoord
 from iris.util import promote_aux_coord_to_dim_coord  # , new_axis
-import aeolus
-from aeolus.io import create_dummy_cube, load_vert_lev
-from aeolus.const import init_const
+import numpy as np
 import pandas as pd
 
-# The ancil regridding script in a separate repo
+filterwarnings("ignore", category=UserWarning)  # noqa
+
+# Third party: ants and the ancil regridding script in a separate repo
+import ants  # noqa: E402
+
 sys.path.append(
     str(
         Path.home()
@@ -28,15 +32,25 @@ sys.path.append(
         / "bin"
     )
 )
-from ancil_regrid_um_to_mesh import main as regrid  # noqa
+from ancil_regrid_um_to_mesh import main as regrid  # noqa: E402
 
 # Local
-import paths  # noqa
-
+import paths  # noqa: E402
 
 CAMEMBERT_GITHUB_RAW = (
     "https://raw.githubusercontent.com/projectcuisines/camembert/main/"
 )
+
+
+def load_socrates_gases(file_path: Path, remove_air=True) -> dict:
+    """Load a dictionary of gas absorbers available in SOCRATES."""
+    import yaml  # noqa
+
+    with file_path.open("r") as f:
+        s_gases = yaml.safe_load(f)
+        if remove_air:
+            _ = s_gases.pop("air")
+    return dict(sorted(s_gases.items(), key=lambda x: x[1]["socrates_ip"]))
 
 
 def dataframe_to_cubelist(df: pd.DataFrame) -> iris.cube.CubeList():
@@ -44,15 +58,15 @@ def dataframe_to_cubelist(df: pd.DataFrame) -> iris.cube.CubeList():
     try:
         from iris.pandas import as_cubes
 
-        cl = as_cubes(df, aux_coord_cols=["Height_m"])
+        cl = as_cubes(df, aux_coord_cols=["height_m"])
     except ImportError:
         cl = iris.cube.CubeList()
         for name in df:
-            if name.startswith("MMR_"):
+            if name.startswith("mmr_"):
                 cube = iris.pandas.as_cube(df[name])
                 cube.add_aux_coord(
                     iris.coords.AuxCoord(
-                        df["Height_m"].values, long_name="Height_m"
+                        df["height_m"].values, long_name="height_m"
                     ),
                     0,
                 )
@@ -64,7 +78,9 @@ def dataframe_to_cubelist(df: pd.DataFrame) -> iris.cube.CubeList():
 
 def load_init_cond(file_path: Path) -> pd.DataFrame:
     """Load the initial profiles into a pandas dataframe."""
-    return pd.read_csv(file_path, delimiter=r"\s+", comment="#")
+    df = pd.read_csv(file_path, delimiter=r"\s+", comment="#")
+    df = df.rename(columns={i: i.lower() for i in df.columns})
+    return df
 
 
 def prep_profiles(
@@ -73,18 +89,18 @@ def prep_profiles(
     """Calculate theta and height and append them to the dataframe."""
     # Discard pressures below the reference pressure
     p_zero = const.reference_surface_pressure.data
-    df.loc[-1, "Pressure_Pa"] = p_zero
-    df = df.sort_values("Pressure_Pa").reset_index(drop=True).interpolate()
-    df = df[df["Pressure_Pa"] <= p_zero]
+    df.loc[-1, "pressure_pa"] = p_zero
+    df = df.sort_values("pressure_pa").reset_index(drop=True).interpolate()
+    df = df[df["pressure_pa"] <= p_zero]
     df = df.reindex(index=df.index[::-1]).reset_index(drop=True)
-    # Calculate additional variables: Exner function, theta, height
+    # Calculate additional variables: exner function, theta, height
     kappa = (
         const.dry_air_gas_constant.data / const.dry_air_spec_heat_press.data
     )
-    df["Exner"] = (df["Pressure_Pa"] / p_zero) ** kappa
-    df["Theta_K"] = df["Temperature_K"] / df["Exner"]
-    pres = df["Pressure_Pa"].values
-    temp = df["Temperature_K"].values
+    df["exner"] = (df["pressure_pa"] / p_zero) ** kappa
+    df["theta_k"] = df["temperature_k"] / df["exner"]
+    pres = df["pressure_pa"].values
+    temp = df["temperature_k"].values
     # Calculate height using the hydrostatic equation and variable gravity
     z = np.zeros(df.shape[0])
     for i in range(1, z.shape[0]):
@@ -94,35 +110,26 @@ def prep_profiles(
         )
         scale_height = const.dry_air_gas_constant.data * temp[i - 1] / g_local
         z[i] = z[i - 1] - scale_height * np.log(pres[i] / pres[i - 1])
-    df["Height_m"] = z
+    df["height_m"] = z
     return df
 
 
 def add_mass_mixing_ratios(
-    df: pd.DataFrame, const: aeolus.const.const.ConstContainer
+    df: pd.DataFrame, gas_dict: dict, const: aeolus.const.const.ConstContainer
 ) -> pd.DataFrame:
     """Convert VMR to MMR and add them to the dataframe."""
-    # data from Socrates: src/radiance_core/gas_list_pcf.F90
-    MOLAR_WEIGHTS = {
-        "H2O": 18.0153,
-        "CO2": 44.01,
-        "CO": 28.0106,
-        "CH4": 16.043,
-        "NH3": 17.0306,
-        "H2": 2.01588,
-        "He": 4.002602,
-    }
-    for gas, gas_mw in MOLAR_WEIGHTS.items():
-        df[f"MMR_{gas}"] = (
-            df[f"VMR_{gas}"]
-            * gas_mw
-            * 1e-3
-            / const.dry_air_molecular_weight.data
-        )
+    for gas, gas_prop in gas_dict.items():
+        if f"vmr_{gas}" in df.columns:
+            df[f"mmr_{gas}"] = (
+                df[f"vmr_{gas}"]
+                * gas_prop["molar_weight"]
+                * 1e-3
+                / const.dry_air_molecular_weight.data
+            )
     return df
 
 
-def make_vert_coord(vert_lev_file_path: Path) -> Tuple[AuxCoord]:
+def make_vert_coord(vert_lev_file_path: Path) -> tuple[AuxCoord]:
     """Create a pair of level height and level number coordinates."""
     # Load vertical levels file
     thlev = load_vert_lev(vert_lev_file_path)
@@ -149,17 +156,17 @@ def interpolate_mmr_in_height(
     """Interpolate MMR cubes in level height."""
     dset_out = iris.cube.CubeList()
     for cube in cube_list:
-        if not cube.name().startswith("MMR"):
+        if not cube.name().startswith("mmr_"):
             # Skip non-MMR cubes
             continue
 
-        cube.var_name = cube.name().lower().lstrip("mmr_")
+        cube.var_name = cube.name().removeprefix("mmr_")
         cube.long_name = f"{cube.var_name}_mmr"
         cube.units = "kg kg-1"
         cube.remove_coord("unknown")
-        promote_aux_coord_to_dim_coord(cube, "Height_m")
-        cube.coord("Height_m").units = "m"
-        cube.coord("Height_m").rename("level_height")
+        promote_aux_coord_to_dim_coord(cube, "height_m")
+        cube.coord("height_m").units = "m"
+        cube.coord("height_m").rename("level_height")
         dset_out.append(
             cube.interpolate(
                 [("level_height", target_theta_lev)], iris.analysis.Linear()
@@ -210,8 +217,51 @@ def broadcast_to_3d(
     return dset_out
 
 
-def main(planet="gj1214b", c_num="C12"):
-    """Main entry point."""
+@click.command()
+@click.argument(
+    "planet",
+    type=click.Choice(["gj1214b", "k2_18b"]),
+)
+@click.argument(
+    "c_num",
+    type=click.Choice([f"C{_c}" for _c in [6, 12, 24, 36, 48, 64]]),
+)
+@click.option(
+    "-l",
+    "--levels",
+    default="Lev_66_4.0e6_uniform",
+    type=click.STRING,
+)
+@click.option(
+    "-z",
+    "--add_missing_as_zeros",
+    is_flag=True,
+    help="Add missing gases with zero values.",
+)
+@click.option(
+    "-v",
+    "--verbose",
+    is_flag=True,
+    help="Show diagnostic messages.",
+)
+def main(
+    planet: str,
+    c_num: str,
+    levels: str,
+    add_missing_as_zeros: bool,
+    verbose: bool,
+) -> None:
+    r"""
+    Prepare CAMEMBERT input data for the UM and LFRic.
+
+    1. Load the data from the GitHub repository\n
+    2. Calculate potential temperature and exner function\n
+    3. Integrate the hydrostatic balance equation to get height\n
+    4. Convert VMR to MMR, opionally set missing gases to 0\n
+    5. Interpolate in the vertical to the specified vertical levels set\n
+    6. Broadcast in the horizontal to the UM grid and save to ancillary file\n
+    7. Regrid from the UM's lat-lon grid to the LFRic's cubed-sphere grid\n
+    """
     # Load the raw data
     filename = f"CAMEMBERT_{planet[:-1].upper()}{planet[-1]}_IC.dat"
     df = load_init_cond(f"{CAMEMBERT_GITHUB_RAW}/InitialConditions/{filename}")
@@ -221,32 +271,19 @@ def main(planet="gj1214b", c_num="C12"):
 
     # Calculate additional thermodynamic variables
     df = prep_profiles(df, const)
-    print(f"theta={np.round(df.Theta_K.values, 2)}")
-    print(f"height={np.round(df.Height_m.values, 2)}")
+    click.secho(f"\ntheta={np.round(df.theta_k.values, 2)}", fg="blue")
+    click.secho(f"\nheight={np.round(df.height_m.values, 2)}", fg="blue")
 
     # Convert VMR to MMR
-    df = add_mass_mixing_ratios(df, const)
+    s_gases = load_socrates_gases(paths.scripts / "gases.yaml")
+    df = add_mass_mixing_ratios(df, s_gases, const)
     max_mmr_sum = (
-        df[
-            [
-                "MMR_H2O",
-                "MMR_CO2",
-                "MMR_CO",
-                "MMR_CH4",
-                "MMR_NH3",
-                "MMR_H2",
-                "MMR_He",
-            ]
-        ]
-        .sum(axis=1)
-        .max()
+        df[[i for i in df.columns if i.startswith("mmr_")]].sum(axis=1).max()
     )
-    print(f"max total MMR: {max_mmr_sum}")
+    click.secho(f"\nmax total MMR: {max_mmr_sum}", fg="blue")
 
     # Create the target vertical grid
-    level_height, model_level_number = make_vert_coord(
-        paths.vert / "Lev_66_4.0e6_uniform"
-    )
+    level_height, model_level_number = make_vert_coord(paths.vert / levels)
     nlev = level_height.shape[0]
 
     # Interpolate in height
@@ -255,74 +292,52 @@ def main(planet="gj1214b", c_num="C12"):
         target_theta_lev=level_height.points,
     )
 
+    # Broadcast horizontally
     dset = broadcast_to_3d(
         dset, level_height=level_height, model_level_number=model_level_number
     )
 
     # Add zero values
-    all_gases = [
-        "cfc11",
-        "cfc113",
-        "cfc12",
-        "ch4",
-        "co",
-        "co2",
-        "dms",
-        "dmso",
-        "h2",
-        "h2o",
-        "h2o2",
-        "h2o2_limit",
-        "h2so4",
-        "hcfc22",
-        "hcn",
-        "he",
-        "hfc134a",
-        "ho2",
-        "monoterpene",
-        "n2",
-        "n2o",
-        "nh3",
-        "no3",
-        "o2",
-        "o3",
-        "oh",
-        "secondary_organic",
-        "so2",
-    ]
-    dset_final = iris.cube.CubeList()
-    for gas_name in all_gases:
-        try:
-            dset_final.append(dset.extract_strict(f"{gas_name}_mmr"))
-        except iris.exceptions.ConstraintMismatchError:
-            print(gas_name)
-            zero_cube = dset[0].copy() * 0.0
-            zero_cube.var_name = gas_name
-            zero_cube.long_name = f"{gas_name}_mmr"
-            dset_final.append(zero_cube)
-    # dset_final = dset
+    if add_missing_as_zeros:
+        note = "all_gases"
+        all_gases = {**s_gases}
+        dset_final = iris.cube.CubeList()
+        for gas_name in all_gases:
+            try:
+                dset_final.append(dset.extract_strict(f"{gas_name}_mmr"))
+            except iris.exceptions.ConstraintMismatchError:
+                if verbose:
+                    click.secho(f"{gas_name:<10} MMR = 0.0", fg="yellow")
+                zero_cube = dset[0].copy() * 0.0
+                zero_cube.var_name = gas_name
+                zero_cube.long_name = f"{gas_name}_mmr"
+                dset_final.append(zero_cube)
+    else:
+        note = "nonzero_only"
+        dset_final = dset
 
     # Define the output file names
     anc_um = (
         paths.ancil
         / "um"
-        / f"camembert_case3_{planet}_gas_mmr_n96e_l{nlev-1}.nc"
+        / f"camembert_case3_{planet}_gas_mmr_n96e_l{nlev-1}_{note}.nc"
     )
     anc_lfric = (
         paths.ancil
         / "lfric"
         / c_num
-        / f"camembert_case3_{planet}_gas_mmr_{c_num}_l{nlev-1}.nc"
+        / f"camembert_case3_{planet}_gas_mmr_{c_num}_l{nlev-1}_{note}.nc"
     )
 
     # Save the data on UM grid
     ants.io.save.netcdf(dset_final, str(anc_um))
+    click.secho(f"\nUM ancil: {anc_um}", fg="green")
 
     # Regrid to LFRic's cubed sphere grid
     tgt_grid = paths.ancil / "lfric" / c_num / "qrparm.orog.ugrid.nc"
     regrid(str(anc_um), str(anc_lfric), target_path=str(tgt_grid))
-    print(f"Result: {anc_lfric}")
+    click.secho(f"\nLFRic ancil: {anc_lfric}", fg="green")
 
 
 if __name__ == "__main__":
-    main(c_num="C48")
+    main()
